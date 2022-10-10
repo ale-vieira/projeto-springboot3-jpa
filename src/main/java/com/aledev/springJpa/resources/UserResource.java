b97cfbbb7447c3475a44bc90dc967c30f5111b95
Alexandre Vieira <alexandrevieira76@gmail.com>
2022-10-10 15:30:28 -0300

    H2 database, test profile, JPA

--- src/main/java/com/aledev/springJpa/resources/UserResource.java
+++ src/main/java/com/aledev/springJpa/resources/UserResource.java
@@ -7,14 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.aledev.springJpa.entities.User;
 
+
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
-		
 }
