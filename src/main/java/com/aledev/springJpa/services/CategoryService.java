23ea35e0ead5a0e07a7d70554eaf9c4c13564c01
Alexandre Vieira <alexandrevieira76@gmail.com>
2022-10-13 15:55:33 -0300

    Category

---
+++ src/main/java/com/aledev/springJpa/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.aledev.springJpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aledev.springJpa.entities.Category;
+import com.aledev.springJpa.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
